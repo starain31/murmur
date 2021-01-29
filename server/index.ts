3ef71cf79b244d049c3af01efc857649e9311303
sakib <nsakib.cse@gmail.com>
2021-01-29 15:12:02 +0600

    Initial commit.

---
+++ server/index.ts
@@ -0,0 +1,42 @@
+import express from 'express'
+import mysql from 'mysql2'
+const app = express();
+
+//mysql setting
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  user: 'docker',
+  password: 'docker',
+  database: 'test'
+});
+
+connection.connect();
+
+//cors setting
+//app.use((req, res, next) => {
+//  res.header("Access-Control-Allow-Origin", "*")
+//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")//
+//  next()
+// })
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Get example
+const router: express.Router = express.Router()
+router.get('/api/getTest', (req, res) => {
+  res.send(req.query)
+})
+
+//Post example
+router.post('/api/postTest', (req, res) => {
+  connection.query('select * from test', function (err, results, fields) {
+    if (err) throw err
+    res.send(results)
+  });
+})
+
+app.use(router)
+
+app.listen(3001, () => { console.log('Example app listening on port 3001!') })
---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Display the data obtained from API here</h1>
+    {{ data }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  async asyncData({ $axios }): Promise<object> {
+    const res = await $axios.$post('http://localhost:3000/api/postTest')
+    console.log(res)
+    return {
+      data: res,
+    }
+  },
+  data() {
+    return {}
+  },
+}
+</script>
