7b47b9122c85f5d931d93cc209e994297fab19e4
sakib <nsakib.cse@gmail.com>
2021-01-31 07:55:23 +0600

    Move murmur details to vue component.

---
+++ client/components/murmur.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ text }}
+    {{ like }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "murmur",
+  props: ["text", "like"]
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-    <h1>MURMURS</h1>
+    <h1>Time Line</h1>
     <ul id="example-1">
       <li v-for="murmur in murmurs" :key="murmur.id">
-        {{ murmur.text }}
-        {{ murmur.like }}
+        <murmur :text="murmur.text" :like="murmur.like"/>
       </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
+import Murmur from "~/components/murmur.vue";
 export default {
+  components: {Murmur},
   async asyncData({ $axios }): Promise<object> {
     const res = await $axios.$get('http://localhost:3001/api/murmurs')
     console.log(res)
