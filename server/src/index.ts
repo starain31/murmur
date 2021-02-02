45fd55a2f6cddfaba7b4f14835a8359b2e5c5a8f
sakib <nsakib.cse@gmail.com>
2021-02-02 17:10:48 +0600

    Resolve express-session warning.

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 app.use(session({
-    secret: 'monu'
+    secret: 'monu',
+    resave: true,
+    saveUninitialized: true
 }))
 
 app.use(router)
