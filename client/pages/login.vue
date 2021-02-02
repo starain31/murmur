c58a52c545d9fc168289aaa486960fdbeb1a0d53
sakib <nsakib.cse@gmail.com>
2021-02-02 11:03:46 +0600

    Add email form.

---
+++ client/pages/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <form action="#" method="post">
+    <h1>Sign in</h1>
+    <section>
+      <label for="email">Email</label>
+      <input id="email" name="email" type="email" autocomplete="username" required autofocus>
+    </section>
+    <section>
+      <label for="current-password">Password</label>
+      <input id="password" name="password" type="password" autocomplete="new-password" required>
+    </section>
+    <button id="sign-in">Sign in</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style scoped>
+
+</style>
