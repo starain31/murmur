9568b0441a8d04246d4d0b40680ed5e8ace31868
sakib <nsakib.cse@gmail.com>
2021-02-03 22:03:32 +0600

    Change timeline.vue to index.vue

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,13 +1,57 @@
 <template>
-  <div></div>
+  <div>
+    <h1>Time Line</h1>
+    <ul id="example-1">
+      <li v-for="murmur in murmurs" :key="murmur.id">
+        <murmur :text="murmur.text" :like="murmur.like" :user="murmur.user"/>
+      </li>
+    </ul>
+    <button v-on:click="previousPage">PREVIOUS PAGE</button>
+    <button v-on:click="nextPage">NEXT PAGE</button>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+import Murmur from "~/components/murmur.vue";
+
 export default {
-  name: "index"
+  name: 'timeline',
+
+  meta: {
+    requiresAuth: true
+  },
+
+  components: {Murmur},
+  methods: {
+    nextPage() {
+      this.page += 1;
+      this.fetchPage();
+    },
+
+    previousPage() {
+      if (this.page > 1) {
+        this.page -= 1;
+        this.fetchPage();
+      }
+    },
+
+    fetchPage(user_id) {
+      return fetch(`http://localhost:3001/api/murmurs?page=${this.page}&user_id=${user_id}`)
+        .then(async (res) => {
+          this.murmurs = await res.json();
+        })
+    }
+  },
+
+  data() {
+    return {
+      murmurs: [],
+      page: 1
+    }
+  },
+
+  mounted() {
+    this.fetchPage(this.$auth.$storage.getUniversal('user_id'))
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
