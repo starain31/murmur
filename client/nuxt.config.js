f716a147948d51ec72d87b257bd6b69227e74b0d
sakib <nsakib.cse@gmail.com>
2021-02-08 22:57:57 +0600

    nuxt auth basic config.

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -53,4 +53,14 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+
+  router: {
+    middleware: ['auth']
+  },
+
+  // auth: {
+  //   strategies: {
+  //     local: { /* ... */ },
+  //   }
+  // }
 }
