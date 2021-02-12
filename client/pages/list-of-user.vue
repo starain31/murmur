5e87d24c0f2dce575bf7c24a19cdf48b14e09cba
sakib <nsakib.cse@gmail.com>
2021-02-12 20:13:43 +0600

    Bugfix: list-of-user.vue page.

--- client/pages/list-of-user.vue
+++ client/pages/list-of-user.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>List of Users</h1>
     <ol id="example-1">
-      <li v-for="user in users" v-if="user.id!==current_user" :key="user.id">
+      <li v-for="user in users" v-if="user.id!==$auth.user.id" :key="user.id">
         <user-details :user="user"/>
         <button v-on:click="follow_user(user.id)">Follow</button>
         <button v-on:click="got_user_page(user.id)">Details</button>
@@ -23,13 +23,12 @@ export default Vue.extend({
   data() {
     return {
       users: [],
-      current_user: this.$auth.$storage.getUniversal('user_id')
     }
   },
 
   methods: {
     update_user() {
-      fetch(`http://localhost:3001/api/users?`)
+      fetch(`http://localhost:3001/api/users`)
         .then(async (response) => {
           this.users = await response.json();
         })
@@ -50,7 +49,7 @@ export default Vue.extend({
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            follower_id: this.$auth.$storage.getUniversal('user_id'),
+            follower_id: this.$auth.user.id,
             following_id: id
           })
         })
