43f3a58dba0519f57f127b966e1710baf8d50999
sakib <nsakib.cse@gmail.com>
2021-02-12 15:32:49 +0600

    Add .env library.

--- server/src/controller/auth.ts
+++ server/src/controller/auth.ts
@@ -2,24 +2,24 @@ import {getRepository} from "typeorm";
 import {User} from "../entity/User";
 import {Request} from 'express';
 import {Auth} from "../entity/auth";
-
-const jwt = require('jsonwebtoken');
-
-const secret = 'SSSSSSSSSS';
+import jwt from "jsonwebtoken";
 
 const login = async (req: any, res: any) => {
     try {
-        console.log()
         await getRepository(Auth).findOneOrFail({
             user_id: req.body.user_id, password: req.body.password
         });
 
         return res.status(200).send({
             auth: true,
-            token: jwt.sign({id: req.body.user_id}, secret, {expiresIn: 86400})
+            token: jwt.sign(
+                {id: req.body.user_id},
+                process.env.AUTH_SECRET as string,
+                {expiresIn: 86400})
         });
 
     } catch (e) {
+        console.error(e);
         return res.status(500).send("There was a problem getting user")
     }
 
@@ -28,15 +28,17 @@ const login = async (req: any, res: any) => {
 
 const user = async (req: Request, res: any) => {
     try {
-        const payload = await jwt.verify(req.header('Authorization'), secret);
-        console.log({payload});
+        const payload = await jwt.verify(
+            req.header('Authorization') as string,
+            process.env.AUTH_SECRET as string,
+        ) as { id: string };
 
         const user = await getRepository(User).findOneOrFail(payload.id);
-
         console.log({user});
         return res.send({user});
     } catch (e) {
         console.error(e);
+        return res.status(500).send("There was a problem getting user")
     }
 
 
