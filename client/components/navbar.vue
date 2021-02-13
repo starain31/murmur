7c4fa6eee2ef8c9810e4b91ceeba22104ae362b5
sakib <nsakib.cse@gmail.com>
2021-02-13 18:03:53 +0600

    Restrict login and logout of navbar according to auth.

--- client/components/navbar.vue
+++ client/components/navbar.vue
@@ -11,11 +11,11 @@
       </div>
       <div class="navbar-menu">
         <div class="navbar-end">
-          <div class="navbar-item has-dropdown is-hoverable">
+          <div class="navbar-item has-dropdown is-hoverable" v-if="$auth.loggedIn">
             <a class="navbar-link">
-              My Account
+              {{ $auth.user.name }}
             </a>
-            <div class="navbar-dropdown">
+            <div class="navbar-dropdown" v-if="this.$auth.loggedIn">
               <nuxt-link class="navbar-item" to="/profile">My Profile</nuxt-link>
               <hr class="navbar-divider"/>
               <a class="navbar-item">Logout</a>
@@ -23,7 +23,7 @@
           </div>
           <template>
             <nuxt-link class="navbar-item" to="/register">Register</nuxt-link>
-            <nuxt-link class="navbar-item" to="/login">Log In</nuxt-link>
+            <nuxt-link class="navbar-item" to="/login" v-if="!this.$auth.loggedIn">Log In</nuxt-link>
           </template>
         </div>
       </div>
